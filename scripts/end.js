6a7ccd45e0cb2ac8686270a9b819f42e570e6d97
buildtrust <buildtrust@google.com>
2021-01-07 08:20:58 +0800

    deploy new contract

--- scripts/end.js
+++ scripts/end.js
@@ -5,14 +5,19 @@ async function main() {
 
     const auction = KeeperAuction.attach(process.env.AUCTION_ADDRESS);
     
-    const result = await auction.end([
-        '0xce38eb60670e1c2b96b8f9413187d4e79ef7a79a',
-        '0x347ba66e4d8bb18601ba03e2b32ceb22c8a8cbd8',
-        '0x8984a423ce4bc23e97c1795ea11a56abecb1ee56',
-        '0xf09b8dda559292111af945e91717da39eef34ade'
-    ]);
-
-    console.log(`Auction ${process.env.AUCTION_ADDRESS} ended at tx: ${result.hash}`);
+    try {
+        const result = await auction.end([
+            '0xdb3443cd328d1774caeb94643419306c7b4a4775',
+            '0xfc8a9d672dd5910c30d567db437dd6fa1fa86d9a',
+            '0x8984a423ce4bc23e97c1795ea11a56abecb1ee56',
+            '0xf09b8dda559292111af945e91717da39eef34ade',
+            '0xce38eb60670e1c2b96b8f9413187d4e79ef7a79a',
+            '0xf4ddd38a2d2f4231e1ddf131a8f3fd80804e08fe'
+        ]);
+        console.log(`Auction ${process.env.AUCTION_ADDRESS} ended at tx: ${result.hash}`);
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 main();
--- scripts/read_states.js
+++ scripts/read_states.js
@@ -11,9 +11,9 @@ async function main() {
     console.log("KeeperAuction bid count:", (await auction.bidCount()).toString());
     console.log("KeeperAuction biddable:", (await auction.biddable()));
     console.log("KeeperAuction withdrawable:", (await auction.withdrawable()));
-    console.log("KeeperAuction cancelable:", (await auction.cancelable(0)));
     console.log("KeeperAuction bidderAmount:", (await auction.bidderAmount('0x8984a423ce4bc23e97c1795ea11a56abecb1ee56')).toString());
     console.log("KeeperAuction userBidsIndex:", (await auction.userBidsIndex('0x8984a423ce4bc23e97c1795ea11a56abecb1ee56')).toString());
+    console.log("KeeperAuction cancelable:", (await auction.cancelable(0)));
 
     const bid0 = await auction.getBid(0);
     console.log(`Bid 0: {
